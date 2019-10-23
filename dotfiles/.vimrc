deb2bcee4d53af4d176ed65578eb577759d02d5c
Francisco Castillo <fjcasti1@asu.edu>
2019-10-23 01:30:57 -0700

    First commit

---
+++ dotfiles/.vimrc
@@ -0,0 +1,193 @@
+" ====================================================== "
+" Use Vundle
+" ====================================================== "
+set nocompatible              " be iMproved, required
+filetype off                  " required
+" set the runtime path to include Vundle and initialize
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+" let Vundle manage Vundle, required
+Plugin 'VundleVim/Vundle.vim'
+" My Plugins "
+Plugin 'powerline/powerline'
+Plugin 'ctrlpvim/ctrlp.vim'
+"Plugin 'python-mode/python-mode'
+Plugin 'davidhalter/jedi-vim'
+Plugin 'ervandew/supertab'
+Plugin 'scrooloose/nerdtree'
+Plugin 'tmhedberg/SimpylFold'
+" All of your Plugins must be added before the following line
+call vundle#end()            " required
+filetype plugin indent on    " required
+" ====================================================== "
+" Use Vundle
+" ====================================================== "
+
+" Performance Options "
+set autoindent " New lines inherit the indentation of previous lines.
+set lazyredraw " Don’t update screen during macro and script execution.
+
+" User Interface Options "
+set title " Sets title of terminal to /path/file - vim
+
+" Automatic reloading of .vimrc "
+autocmd! bufwritepost .vimrc source %
+
+" Better Copy & Paste "
+set pastetoggle=<F2>
+set clipboard=unnamed
+
+" Rebind <Leader> Key "
+let mapleader = " "
+
+"Backspace behaves normally"
+set backspace=indent,eol,start
+
+" Tabs "
+map <Leader>t :tabnew<CR>
+map <Leader>n :tabnext<CR>
+map <Leader>b :tabprevious<CR>
+
+" Horizontal splits "
+map <Leader>h :split<CR>
+map <Leader><Up> <c-w><Up><CR>
+map <Leader><Down> <c-w><Down><CR>
+
+" Vertical splits "
+map <Leader>v :vsplit<CR>
+map <Leader><Left> <c-w><Left><CR>
+map <Leader><Right> <c-w><Right><CR>
+
+" Save & Quit Quick "
+noremap <Leader>q :q<CR>
+noremap <Leader>w :w<CR>
+
+" Map sort function to a key "
+vnoremap <Leader>s :sort<CR>
+
+" Easier moving of code blocks, aka it won't lose its selection "
+vnoremap < <gv
+vnoremap > >gv
+
+" Show whitespace "
+" MUST be inserted BEFORE the colorscheme command "
+autocmd ColorScheme * highlight ExtraWhitespace ctermbg=red guibg=red
+au InsertLeave * match ExtraWhitespace /\s\+$/
+
+" Colorscheme "
+set t_Co=256
+color lichen
+
+" Syntax Highlight "
+filetype on
+filetype plugin indent on
+syntax on
+
+"" Showing line numbers and length "
+set number " Number lines "
+set tw=79  " width of document (used by gd) "
+set nowrap " don't automatically wrap on load "
+set fo-=t  " don't automatically wrap text when typing "
+set colorcolumn=79
+"" Don't write your code longer than 80 characters, first line warning "
+"if exists('+colorcolumn')
+"  execute "set colorcolumn=72," . join(range(80,300),",")
+"endif
+highlight ColorColumn ctermbg=233
+
+" Easier formatting of paragraphs "
+vmap Q gq
+nmap Q gqap
+
+" Useful settings "
+set history=1000
+set undolevels=700
+
+" Real programmers don't use TABs but spaces "
+set tabstop=4
+set softtabstop=4
+set shiftwidth=4
+set shiftround
+set expandtab
+
+" Make search case insensitive "
+set hlsearch
+set ignorecase
+set incsearch
+set smartcase
+
+" Shows info on current command in status line "
+set showcmd
+
+" Allows for more robust behavior of <%> matching.
+" " NOTE -- runtime vs. source -- runtime is relative to vim directory;
+"   while source needs an absolute path.
+runtime macros/matchit.vim
+
+" Shows list for <tab> complete in : (command) mode
+set wildmenu
+set wildmode=list:longest " Shows all of the options in wild menu.
+
+
+" ===================================================================== "
+" Python IDE Setup "
+" ===================================================================== "
+"
+" Settings for vim-powerline "
+set laststatus=2
+
+" Settings for ctrlP "
+map <Leader>o <C-p>
+let g:ctrlp_max_height = 20
+let g:ctrlp_working_path_mode = 'ra'
+set wildignore+=*.pyc
+set wildignore+=*_build/*
+set wildignore+=*/coverage/*
+
+" Setting for python-mode "
+"map <Leader>g :call RopeGotoDefinition()<CR>
+"let ropevim_enable_shortcuts=1
+"let g:pymode_rope_goto_def_newwin="vnew"
+"let g:pymode_rope_extended_complete=1
+"let g:pymode_breakpoint=0
+"let g:pymode_syntax=1
+"let g:pymode_syntax_builtin_objs=0
+"let g:pymode_syntax_builtin_funcs=0
+"map <Leader>k Oimport ipdb; ipdb.set_trace() # CHECKPOINT<C-c>
+
+"set completeopt=longest,menuone
+"function! OmniPopup(action)
+"    if pumvisible()
+"        if a:action=='j'
+
+" Python Folding
+set nofoldenable
+nnoremap fo zo
+nnoremap fO zR
+nnoremap ff zc
+nnoremap fF zM
+highlight Folded guibg=grey guifg=blue
+highlight FoldColumn guibg=darkgrey guifg=white
+set smartindent           " Indents instead of tabs
+"set foldmethod=indent
+"set foldlevel=99
+
+"" SuperTab "
+au FileType python set omnifunc=pythoncomplete#Complete
+let g:SuperTabDefaultCompletionType="context"
+set completeopt=menuone,longest,preview
+
+
+let g:jedi#auto_initialization = 1
+let g:jedi#smart_auto_mappings = 1
+let g:jedi#max_doc_height=50
+let g:jedi#usages_command=""
+let g:jedi#popup_select_first = 1
+
+" NERDTree "
+map <Leader>a :NERDTree<CR>
+map <Leader>' :NERDTreeClose<CR>
+let NERDTreeQuitOnOpen=3
+let NERDTreeNaturalSort=1
+let NERDTreeCustomOpenArgs={'file':{'where':'t'}, 'dir':{'where':'t'}}
+
---
+++ utilities/cleanDownloads.py
@@ -0,0 +1,217 @@
+from datetime import datetime
+from os.path import isfile, join
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+import os
+import time
+
+
+folder_to_track = "/home/kiko/Downloads"
+folder_destination = folder_to_track
+
+
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        files = [f for f in os.listdir(folder_to_track) if
+                 isfile(join(folder_to_track, f))]
+        for filename in files:
+            i = 1
+            try:
+                extension = str(os.path.splitext(folder_to_track + '/' +
+                                                 filename)[-1])
+            except Exception:
+                extension = 'noname'
+
+            if extension == '.crdownload':
+                break
+            if extension not in extensions_dict:
+                extension = 'noname'
+
+            now = datetime.now()
+            year = now.strftime("%Y")
+
+            folder_destination_path = extensions_dict[extension] + "/" + year
+
+            if not os.path.exists(folder_destination_path):
+                os.makedirs(folder_destination_path, exist_ok=True)
+
+            srcfile = folder_to_track + "/" + filename
+            file_exists = os.path.isfile(folder_destination_path + "/" +
+                                         filename)
+            while file_exists:
+                i += 1
+                filename = os.path.splitext(folder_to_track + '/' +
+                                            filename)[0] + '__' + str(i) + \
+                    os.path.splitext(folder_to_track + '/' + filename)[-1]
+                filename = filename.split("/")[-1]
+                file_exists = os.path.isfile(folder_destination_path + "/" +
+                                             filename)
+
+            filename = folder_destination_path + "/" + filename
+            os.rename(srcfile, filename)
+
+
+extensions_dict = {
+    # No name
+    'noname': folder_destination + "/Uncategorized",
+    # Temporary/Downloading Files
+    '.crdownload': folder_destination + "/",
+    # Audio
+    '.aif':   folder_destination + "/Media/Audio/",
+    '.cda':   folder_destination + "/Media/Audio/",
+    '.mid':   folder_destination + "/Media/Audio/",
+    '.midi':  folder_destination + "/Media/Audio/",
+    '.mp3':   folder_destination + "/Media/Audio/",
+    '.mpa':   folder_destination + "/Media/Audio/",
+    '.ogg':   folder_destination + "/Media/Audio/",
+    '.wav':   folder_destination + "/Media/Audio/",
+    '.wma':   folder_destination + "/Media/Audio/",
+    '.wpl':   folder_destination + "/Media/Audio/",
+    '.m3u':   folder_destination + "/Media/Audio/",
+    # Text
+    '.txt':   folder_destination + "/Text/TextFiles/",
+    '.doc':   folder_destination + "/Text/Microsoft/Word/",
+    '.docx':  folder_destination + "/Text/Microsoft/Word/",
+    '.odt':   folder_destination + "/Text/TextFiles/",
+    '.pdf':   folder_destination + "/Text/PDF/",
+    '.rtf':   folder_destination + "/Text/TextFiles/",
+    '.tex':   folder_destination + "/Text/TextFiles/",
+    '.wks':   folder_destination + "/Text/TextFiles/",
+    '.wps':   folder_destination + "/Text/TextFiles/",
+    '.wpd':   folder_destination + "/Text/TextFiles/",
+    # Video
+    '.3g2':   folder_destination + "/Media/Video/",
+    '.3gp':   folder_destination + "/Media/Video/",
+    '.avi':   folder_destination + "/Media/Video/",
+    '.flv':   folder_destination + "/Media/Video/",
+    '.h264':  folder_destination + "/Media/Video/",
+    '.m4v':   folder_destination + "/Media/Video/",
+    '.mkv':   folder_destination + "/Media/Video/",
+    '.mov':   folder_destination + "/Media/Video/",
+    '.mp4':   folder_destination + "/Media/Video/",
+    '.mpg':   folder_destination + "/Media/Video/",
+    '.mpeg':  folder_destination + "/Media/Video/",
+    '.rm':    folder_destination + "/Media/Video/",
+    '.swf':   folder_destination + "/Media/Video/",
+    '.vob':   folder_destination + "/Media/Video/",
+    '.wmv':   folder_destination + "/Media/Video/",
+    # Image
+    '.ai':    folder_destination + "/Media/Images/",
+    '.bmp':   folder_destination + "/Media/Images/",
+    '.gif':   folder_destination + "/Media/Images/",
+    '.ico':   folder_destination + "/Media/Images/",
+    '.jpg':   folder_destination + "/Media/Images/",
+    '.jpeg':  folder_destination + "/Media/Images/",
+    '.png':   folder_destination + "/Media/Images/",
+    '.ps':    folder_destination + "/Media/Images/",
+    '.psd':   folder_destination + "/Media/Images/",
+    '.svg':   folder_destination + "/Media/Images/",
+    '.tif':   folder_destination + "/Media/Images/",
+    '.tiff':  folder_destination + "/Media/Images/",
+    '.CR2':   folder_destination + "/Media/Images/",
+    # Interne t
+    '.asp':   folder_destination + "/Other/Internet/",
+    '.aspx':  folder_destination + "/Other/Internet/",
+    '.cer':   folder_destination + "/Other/Internet/",
+    '.cfm':   folder_destination + "/Other/Internet/",
+    '.cgi':   folder_destination + "/Other/Internet/",
+    '.pl':    folder_destination + "/Other/Internet/",
+    '.css':   folder_destination + "/Other/Internet/",
+    '.htm':   folder_destination + "/Other/Internet/",
+    '.js':    folder_destination + "/Other/Internet/",
+    '.jsp':   folder_destination + "/Other/Internet/",
+    '.part':  folder_destination + "/Other/Internet/",
+    '.php':   folder_destination + "/Other/Internet/",
+    '.rss':   folder_destination + "/Other/Internet/",
+    '.xhtml': folder_destination + "/Other/Internet/",
+    # Compressed
+    '.7z':    folder_destination + "/Other/Compressed/",
+    '.arj':   folder_destination + "/Other/Compressed/",
+    '.deb':   folder_destination + "/Other/Compressed/",
+    '.pkg':   folder_destination + "/Other/Compressed/",
+    '.rar':   folder_destination + "/Other/Compressed/",
+    '.rpm':   folder_destination + "/Other/Compressed/",
+    '.tar.gz': folder_destination + "/Other/Compressed/",
+    '.z':     folder_destination + "/Other/Compressed/",
+    '.zip':   folder_destination + "/Other/Compressed/",
+    # Disc
+    '.bin':   folder_destination + "/Other/Disc/",
+    '.dmg':   folder_destination + "/Other/Disc/",
+    '.iso':   folder_destination + "/Other/Disc/",
+    '.toast': folder_destination + "/Other/Disc/",
+    '.vcd':   folder_destination + "/Other/Disc/",
+    # Data
+    '.csv':   folder_destination + "/Programming/Database/",
+    '.dat':   folder_destination + "/Programming/Database/",
+    '.db':    folder_destination + "/Programming/Database/",
+    '.dbf':   folder_destination + "/Programming/Database/",
+    '.log':   folder_destination + "/Programming/Database/",
+    '.mdb':   folder_destination + "/Programming/Database/",
+    '.sav':   folder_destination + "/Programming/Database/",
+    '.sql':   folder_destination + "/Programming/Database/",
+    '.tar':   folder_destination + "/Programming/Database/",
+    '.xml':   folder_destination + "/Programming/Database/",
+    '.json':  folder_destination + "/Programming/Database/",
+    # Executables
+    '.apk':   folder_destination + "/Other/Executables/",
+    '.bat':   folder_destination + "/Other/Executables/",
+    '.com':   folder_destination + "/Other/Executables/",
+    '.exe':   folder_destination + "/Other/Executables/",
+    '.gadget': folder_destination + "/Other/Executables/",
+    '.jar':   folder_destination + "/Other/Executables/",
+    '.wsf':   folder_destination + "/Other/Executables/",
+    # Fonts
+    '.fnt':   folder_destination + "/Other/Fonts/",
+    '.fon':   folder_destination + "/Other/Fonts/",
+    '.otf':   folder_destination + "/Other/Fonts/",
+    '.ttf':   folder_destination + "/Other/Fonts/",
+    # Presentations
+    '.key':   folder_destination + "/Presentations/",
+    '.odp':   folder_destination + "/Presentations/",
+    '.pps':   folder_destination + "/Presentations/",
+    '.ppt':   folder_destination + "/Presentations/",
+    '.pptx':  folder_destination + "/Presentations/",
+    # Programming
+    '.c':     folder_destination + "/Programming/C&C++/",
+    '.class': folder_destination + "/Programming/Java/",
+    '.dart':  folder_destination + "/Programming/Dart/",
+    '.py':    folder_destination + "/Programming/Python/",
+    '.sh':    folder_destination + "/Programming/Shell/",
+    '.f':     folder_destination + "/Programming/Fortran/",
+    '.f90':   folder_destination + "/Programming/Fortran/",
+    '.swift': folder_destination + "/Programming/Swift/",
+    '.html':  folder_destination + "/Programming/html/",
+    '.h':     folder_destination + "/Programming/C&C++/",
+    # Spreadsheets
+    '.ods':   folder_destination + "/SpreadSheets/",
+    '.xlr':   folder_destination + "/SpreadSheets/",
+    '.xls':   folder_destination + "/SpreadSheets/",
+    '.xlsx':  folder_destination + "/SpreadSheets/",
+    # System
+    '.bak':   folder_destination + "/Text/Other/System/",
+    '.cab':   folder_destination + "/Text/Other/System/",
+    '.cfg':   folder_destination + "/Text/Other/System/",
+    '.cpl':   folder_destination + "/Text/Other/System/",
+    '.cur':   folder_destination + "/Text/Other/System/",
+    '.dll':   folder_destination + "/Text/Other/System/",
+    '.dmp':   folder_destination + "/Text/Other/System/",
+    '.drv':   folder_destination + "/Text/Other/System/",
+    '.icns':  folder_destination + "/Text/Other/System/",
+    '.ini':   folder_destination + "/Text/Other/System/",
+    '.lnk':   folder_destination + "/Text/Other/System/",
+    '.msi':   folder_destination + "/Text/Other/System/",
+    '.sys':   folder_destination + "/Text/Other/System/",
+    '.tmp':   folder_destination + "/Text/Other/System/",
+}
+
+event_handler = MyHandler()
+observer = Observer()
+observer.schedule(event_handler, folder_to_track, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(10)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
