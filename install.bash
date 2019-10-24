b569843ad71d27021b0fe8356ca970f93b82df50
Francisco Castillo <fjcasti1@asu.edu>
2019-10-23 22:58:03 -0700

    install.bash created

---
+++ install.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#
+# Installs configuration files to User's home directory
+# ----------------------------------------------------------------------
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp ~/.myconfig/dotfiles/.vimrc ~/
+
