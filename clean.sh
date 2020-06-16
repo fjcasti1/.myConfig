3e624078fa9f5ecc59fd9486454affed80792610
Francisco Castillo <fjcasti1@asu.edu>
2020-06-16 16:42:58 -0700

    clean added gitignore and exports

--- clean.sh
+++ clean.sh
@@ -3,8 +3,8 @@
 # Installs configuration files to User's home directory
 # ----------------------------------------------------------------------
 
-for file in {bashrc,bash_profile,bash_prompt,aliases,functions,inputrc,vimrc}; do
-	[ -f ~/.$file -o -L ~/.$file ] && rm ~/.$file;
+for file in {bashrc,bash_profile,bash_prompt,aliases,functions,inputrc,vimrc,gitconfig,exports}; do
+  [ -f ~/.$file -o -L ~/.$file ] && rm ~/.$file;
 done;
 unset file;
 
