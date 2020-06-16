5e34d1ae01e1f140aa12f1bac9bd3d1a036fe5c0
Francisco Castillo <fjcasti1@asu.edu>
2020-06-16 14:38:24 -0700

    script to clean all introduced files

---
+++ clean.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# Installs configuration files to User's home directory
+# ----------------------------------------------------------------------
+
+for file in {bashrc,bash_profile,bash_prompt,aliases,functions,inputrc,vimrc}; do
+	[ -f ~/.$file -o -L ~/.$file ] && rm ~/.$file;
+done;
+unset file;
+
+# Remove .vim directory
+rm -rf ~/.vim
